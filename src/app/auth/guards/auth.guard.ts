cb2f7fb1365115cf0f3085a222658209cc5e93cf
Josue Baez <josue.baez@coderhouse.com>
2023-01-25 09:47:04 -0300

    FIX, verify token

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -11,7 +11,6 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    this.authService.verifyToken();
     return this.authService.isAuthenticated$
       .pipe(
         tap((isAuthenticated) => {
